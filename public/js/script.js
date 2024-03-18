1957a551f657d318d2972ef98e1ac2afd18939c3
Anthonysales902 <139911668+Anthonysales902@users.noreply.github.com>
2024-03-18 13:19:21 -0300

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-
+document.querySelector('#button').addEventListener('click', () => {
+    if (document.querySelector('input[name="searchParameter"]:checked').value === 'title') {
+            const titulo = document.querySelector('#title').value
+            window.location.assign('/search?titulo=' + titulo); 
+        } else {
+            const ano = document.querySelector('#year').value
+            window.location.assign('/search/' + ano); 
+        }
+    })
